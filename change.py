569c8bcbf967d1485b56a230bb7241c37a2960ce
carward25 <carward@ucsc.edu>
2021-10-06 23:05:25 -0700

    # This is the project for part c of assignment 2

---
+++ change.py
@@ -0,0 +1,14 @@
+#
+C = int(input("Please enter an amount in cents less than a dollar: "))
+
+print("Your change will be: ",)
+
+print("Q: ", C//25)
+C = C%25
+print("D :", C//10)
+C = C%10
+print("N: ", C//5)
+C = C%5
+print("P: ", C//1)
+C = C%1
+
